3d540a8cc9c6f816b241913042008f09323455af
Matteo Redaelli <matteo@debian.fritz.box>
2023-11-09 22:11:21 +0100

    license

--- artifacts/sql-sqlline.el
+++ artifacts/sql-sqlline.el
@@ -2,8 +2,7 @@
 
 ;; Copyright (C) since 2019 Matteo Redaelli
 ;; Author: Matteo Redaelli <matteo.redaelli@gmail.com>
-
-;; Version: 1.0.2
+;; Version: 1.0.3
 ;; Keywords: languages
 ;; Package-Requires: ((emacs "24.4"))
 ;; Homepage: https://gitlab.com/matteoredaelli/sql-sqlline
@@ -58,6 +57,19 @@
 
 
 ;; * License
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;   Please see the LICENSE file.
 
